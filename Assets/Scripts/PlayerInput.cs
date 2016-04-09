0799b3151d766764ea19cec3b9cd7b547d2b6577
elberdev <elberdev@gmail.com>
2016-04-09 08:52:45 -0400

    first commit

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// our custom struct for our own simplified touch controls
+	public struct SimpleTouch {
+	
+		public Vector2 StartTouchLocation;
+		public Vector2 CurrentTouchLocation;
+		public DateTime StartTime;
+		// we will use this to compare the default touch input from Unity with
+		// our SimpleTouch
+		public TouchPhase Phase;
+	}
+
+	// these will allow us to finetune the swipe in the editor
+	public float SwipeTime;
+	public float SwipeDistance;
+
+	// variables for us to link unity's raw touch input to our custom touch
+	private SimpleTouch ActiveTouch;
+	private Touch DeviceTouch;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
