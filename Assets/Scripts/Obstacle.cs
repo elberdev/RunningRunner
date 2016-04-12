0c8afa3c56cd213d4f6fac835b1e3023e43fde06
elberdev <elberdev@gmail.com>
2016-04-11 23:33:02 -0400

    added Obstacle script for axe rotation

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class Obstacle : MonoBehaviour {
 
+	public Transform pivot;
+
+	// random range for rotation speed x will be min and y max
+	public Vector2 rotationSpeedGap;
+
+	// actual speed based on rotationSpeedGap
+	private float rotationSpeed;
+
 	// Use this for initialization
 	void Start () {
 	
+		rotationSpeed = Random.Range (rotationSpeedGap.x, rotationSpeedGap.y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		// rotate axe in accordance with our variables. Vector3.forward is
+		// shorthand for (0, 0, 1). Rotation here happens along the z axis
+		pivot.transform.Rotate(Vector3.forward, rotationSpeed);
+
 	}
 }
