55cb70e1b61614929f6aacb5bab4daeb77d486bd
elberdev <elberdev@gmail.com>
2016-04-12 12:36:36 -0400

    finished gui for sprite tile editor window and added button logic to validate values before creating grid

--- Assets/Scripts/SpriteTiler.cs
+++ Assets/Scripts/SpriteTiler.cs
@@ -25,4 +25,72 @@ public class SpriteTiler : EditorWindow {
 		// this opens a new editor window! Cool bean, huh?!
 		EditorWindow.GetWindow<SpriteTiler> (true, "Sprite Tiler");
 	}
+
+	// called to render GUI frames and elements
+	void OnGUI() {
+	
+		// setting for the game object that holds our tiled objects
+		// creates a text label
+		GUILayout.Label ("Tile Level Object Name", EditorStyles.boldLabel);
+		// sets a textField with a default value that can be used for the user to 
+		// change the value of the variable at any time.
+		tileSpriteRootGameObjectName = GUILayout.TextField (tileSpriteRootGameObjectName, 25);
+
+		// slider for x grid value (left to right)
+		GUILayout.Label("X: " + gridXSlider, EditorStyles.boldLabel);
+		gridXSlider = GUILayout.HorizontalScrollbar (gridXSlider, 1.0f, 0.0f, 30.0f);
+		gridXSlider = (int)gridXSlider;
+
+		// slider for y grid value (up to down)
+		GUILayout.Label("Y: " + gridYSlider, EditorStyles.boldLabel);
+		gridYSlider = GUILayout.HorizontalScrollbar (gridYSlider, 1.0f, 0.0f, 30.0f);
+		gridYSlider = (int)gridYSlider;
+
+		// file chosen to be our ground sprite
+		GUILayout.Label("Sprite Ground File", EditorStyles.boldLabel);
+		tileGroundSprite = EditorGUILayout.ObjectField (tileGroundSprite, typeof(Sprite), true) as Sprite;
+
+		// file chose to be our dirt sprite
+		GUILayout.Label("Sprite Dirt File", EditorStyles.boldLabel);
+		tileDirtSprite = EditorGUILayout.ObjectField (tileDirtSprite, typeof(Sprite), true) as Sprite;
+
+		// if button "create tiled" is pressed
+		if (GUILayout.Button ("Create Tiled")) {
+		
+			// if the grid settings are both 0, send notification to user
+			if (gridXSlider == 0 && gridYSlider == 0) {
+			
+				ShowNotification (new GUIContent ("Must have either x of y grid set to a value greater than 0"));
+				return;
+			}
+
+			// if tile and dirt sprites exist
+			if (tileDirtSprite != null && tileGroundSprite != null) {
+			
+				// if the sprite sizes don't match, show a notification to the user
+				if (tileDirtSprite.bounds.size.x != tileGroundSprite.bounds.size.x ||
+				    tileDirtSprite.bounds.size.y != tileGroundSprite.bounds.size.y) {
+				
+					ShowNotification (new GUIContent ("Both sprites must be of matching size"));
+					return;
+				}
+
+				// create game object and tiled objects with user settings
+				CreateSpriteTiledGameObject (gridXSlider, gridYSlider, 
+					tileGroundSprite, tileDirtSprite, tileSpriteRootGameObjectName);
+				
+			} else {
+
+				// if either dirt or ground sprite don't exist, send notification to user
+				ShowNotification(new GUIContent("Must have Dirt and Ground sprites selected"));
+				return;
+			}
+		}
+	}
+
+	public static void CreateSpriteTiledGameObject (float gridXSlider, float gridYSlider, 
+		Sprite tileGroundSprite, Sprite tileDirtSprite, string tileSpriteRootGameObjectName) {
+	
+		
+	}
 }
