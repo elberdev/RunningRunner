9835faa4687c0f28553174c92d177756edd55782
elberdev <elberdev@gmail.com>
2016-04-12 13:48:11 -0400

    finished gui tile editor creator functionality, created a few coin pattern prefabs

--- Assets/Scripts/SpriteTiler.cs
+++ Assets/Scripts/SpriteTiler.cs
@@ -60,7 +60,8 @@ public class SpriteTiler : EditorWindow {
 			// if the grid settings are both 0, send notification to user
 			if (gridXSlider == 0 && gridYSlider == 0) {
 			
-				ShowNotification (new GUIContent ("Must have either x of y grid set to a value greater than 0"));
+				ShowNotification (new GUIContent (
+					"Must have either x of y grid set to a value greater than 0"));
 				return;
 			}
 
@@ -91,6 +92,62 @@ public class SpriteTiler : EditorWindow {
 	public static void CreateSpriteTiledGameObject (float gridXSlider, float gridYSlider, 
 		Sprite tileGroundSprite, Sprite tileDirtSprite, string tileSpriteRootGameObjectName) {
 	
+		// store the size of the sprite
+		float spriteX = tileGroundSprite.bounds.size.x;
+		float spriteY = tileGroundSprite.bounds.size.y;
+
+		// create the root game object which will hold children
+		GameObject rootObject = new GameObject();
+
+		// set position in world to (0,0,0)
+		rootObject.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+
+		// name it based on our user settings
+		rootObject.name = tileSpriteRootGameObjectName;
+
+		// create starting values for a while loop
+		int currentObjectCount = 0;
+		int currentColumn = 0;
+		int currentRow = 0;
+		Vector3 currentLocation = new Vector3 (0.0f, 0.0f, 0.0f);
+
+		// continue loop until all row and column cells have been filled
+		while (currentRow < gridYSlider) {
 		
+			// create a child game object, set its parent to root, name it, 
+			// and offset its location based on current location
+			GameObject gridObject = new GameObject();
+			gridObject.transform.SetParent (rootObject.transform);
+			gridObject.name = rootObject.name + "_" + currentObjectCount;
+			gridObject.transform.position = currentLocation;
+
+			// give child gridObject a SpriteRenderer and set Sprite on current row
+			SpriteRenderer gridRenderer = gridObject.AddComponent<SpriteRenderer>();
+			gridRenderer.sprite = (currentRow == 0) ? tileGroundSprite : tileDirtSprite;
+
+			// give the grid object a box collider. If we added it to gridRenderer
+			// instead, it would be too big
+			gridObject.AddComponent<BoxCollider2D>();
+
+			// offset current location by the size of each cell (sprite) 
+			// for the next gridObject 
+			currentLocation.x += spriteX;
+
+			// increment current column by one;
+			currentColumn++;
+
+			// if current column becomes greater than the x slider
+			if (currentColumn >= gridXSlider) {
+			
+				// reset column, increment row, reset x location
+				currentColumn = 0;
+				currentRow++;
+				currentLocation.x = 0;
+				currentLocation.y -= spriteY;
+			}
+
+			// add to the current object count
+			currentObjectCount++;
+		}
 	}
 }
