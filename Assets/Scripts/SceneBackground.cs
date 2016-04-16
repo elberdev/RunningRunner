80b2dfc8257ccadd84faf5ba1bf10abef9f25f42
elberdev <elberdev@gmail.com>
2016-04-16 17:43:39 -0400

    filled in start function of SceneBackground to set up and dispaly background sprites

--- Assets/Scripts/SceneBackground.cs
+++ Assets/Scripts/SceneBackground.cs
@@ -29,6 +29,28 @@ public class SceneBackground : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	
+		SpriteRenderer backgroundRenderer;
+		GameObject backgroundObject;
+
+		// for each scene background element set in inspector
+		for (int i = 0; i < sceneBackgroundElement.Length; i++) {
+		
+			// create new game object
+			backgroundObject = new GameObject ();
+
+			// parent it, set its position and scale according to inspector settings
+			backgroundObject.transform.SetParent(transform);
+			backgroundObject.transform.position = sceneBackgroundElement [i].spriteLocation;
+			backgroundObject.transform.localScale = sceneBackgroundElement [i].spriteScale;
+
+			// keep reference to created new object and its start location
+			sceneBackgroundElement[i].childObject = backgroundObject;
+			sceneBackgroundElement[i].objectStartLocation = backgroundObject.transform.position;
+
+			// create renderer and set sprite
+			backgroundRenderer = backgroundObject.AddComponent<SpriteRenderer>();
+			backgroundRenderer.sprite = sceneBackgroundElement [i].backgroundSprite;
+		}
 	}
 	
 	// Update is called once per frame
