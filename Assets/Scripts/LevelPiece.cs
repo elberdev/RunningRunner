e9aabf8f3fd4aa852159b8b6f18d64c91891dd4b
elberdev <elberdev@gmail.com>
2016-04-16 00:22:08 -0400

    created LevelPiece script to keep track of Level Piece locations

---
+++ Assets/Scripts/LevelPiece.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelPiece : MonoBehaviour {
+
+	// Initial location of the level piece
+	private Vector3 initialLocation;
+
+	// Use this for initialization
+	void Awake () {
+		
+		initialLocation = transform.position;
+	}
+	
+	// get initial location of the level piece
+	public Vector3 GetInitialLocation() {
+	
+		return initialLocation;
+	}
+}
