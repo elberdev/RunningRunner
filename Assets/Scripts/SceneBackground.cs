6b4657d06c30874e4e3a4c1da320f27d35d4d419
elberdev <elberdev@gmail.com>
2016-04-16 20:09:18 -0400

    added some movement to background elements. I dont like the backwards movement at all so I will change the movement to more of a regular parallax scroll at some point in the future

--- Assets/Scripts/SceneBackground.cs
+++ Assets/Scripts/SceneBackground.cs
@@ -24,7 +24,7 @@ public class SceneBackground : MonoBehaviour {
 	};
 
 	// array to store backgrounds for scene
-	public BackgroundElement[] sceneBackgroundElement;
+	public BackgroundElement[] sceneBackgroundElements;
 
 	// Use this for initialization
 	void Start () {
@@ -33,28 +33,53 @@ public class SceneBackground : MonoBehaviour {
 		GameObject backgroundObject;
 
 		// for each scene background element set in inspector
-		for (int i = 0; i < sceneBackgroundElement.Length; i++) {
+		for (int i = 0; i < sceneBackgroundElements.Length; i++) {
 		
 			// create new game object
 			backgroundObject = new GameObject ();
 
 			// parent it, set its position and scale according to inspector settings
 			backgroundObject.transform.SetParent(transform);
-			backgroundObject.transform.position = sceneBackgroundElement [i].spriteLocation;
-			backgroundObject.transform.localScale = sceneBackgroundElement [i].spriteScale;
+			backgroundObject.transform.position = sceneBackgroundElements [i].spriteLocation;
+			backgroundObject.transform.localScale = sceneBackgroundElements [i].spriteScale;
 
 			// keep reference to created new object and its start location
-			sceneBackgroundElement[i].childObject = backgroundObject;
-			sceneBackgroundElement[i].objectStartLocation = backgroundObject.transform.position;
+			sceneBackgroundElements[i].childObject = backgroundObject;
+			sceneBackgroundElements[i].objectStartLocation = backgroundObject.transform.position;
 
 			// create renderer and set sprite
 			backgroundRenderer = backgroundObject.AddComponent<SpriteRenderer>();
-			backgroundRenderer.sprite = sceneBackgroundElement [i].backgroundSprite;
+			backgroundRenderer.sprite = sceneBackgroundElements [i].backgroundSprite;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		for (int i = 0; i < sceneBackgroundElements.Length; i++) {
+		
+			// if the background element is supposed to move
+			if (sceneBackgroundElements [i].movementSpeed > 0) {
+			
+				// get current location and offset it by movement speed based on bMoveBackwards
+				Vector3 newSceneBackgroundLocation = sceneBackgroundElements[i].childObject.transform.position;
+				newSceneBackgroundLocation.x += (sceneBackgroundElements [i].bMoveBackwards) 
+					? -sceneBackgroundElements [i].movementSpeed * Time.deltaTime
+					: sceneBackgroundElements [i].movementSpeed * Time.deltaTime;
+
+				// set location based on movement speed offset
+				sceneBackgroundElements[i].childObject.transform.position = newSceneBackgroundLocation;
+
+				// if child object has moved further than the move distance have it move
+				// the other way
+				if (Vector3.Distance (sceneBackgroundElements [i].childObject.transform.position, 
+					sceneBackgroundElements [i].objectStartLocation) >= sceneBackgroundElements [i].movementDistance) 
+				{
+					// change the direction of the scene background element
+					sceneBackgroundElements[i].bMoveBackwards = !sceneBackgroundElements[i].bMoveBackwards;
+				}
+
+			}
+		}
 	}
 }
